207c1a2a87786ffa3e956bc73a7cd33b8af0f4f6
MK <MK@ingenio-3.local>
2022-04-21 09:31:30 +0200

    testing

--- 2_2_MySQL_queries/universidad_queries.sql
+++ 2_2_MySQL_queries/universidad_queries.sql
@@ -1,3 +1,6 @@
+
+
+-- funciona ?
 USE schema_universidad
 
 ---- Retorna un llistat amb el primer cognom, segon cognom i el nom de tots els alumnes. El llistat haurà d'estar ordenat alfabèticament de menor a major pel primer cognom, segon cognom i nom.
